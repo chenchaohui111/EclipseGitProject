f5f79fcae707f466776556f36f46c81b4dc92131
chenchaohui111 <1002443655@qq.com>
2020-02-20 10:01:55 +0800

    第四次提交

--- src/collection/TestList.java
+++ src/collection/TestList.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
-//123334
+//1233345
 public class TestList {
 	/**
 	 * List 特点：①有序 ②可重复
